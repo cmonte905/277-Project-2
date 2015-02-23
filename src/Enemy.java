afe4676c076c3f5f530c21bb1721a3f3e25b9e06
cmonte905 <cmonte905@hotmail.com>
2015-02-23 15:40:54 -0800

    New version V.2

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,22 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-public class Enemy extends Character{
+public class Enemy extends Character {
 	private Item item;
+
 	public Enemy(String n, String q, int h, int l, int g, Item i) {
 		super(n, q, h, l, g);
 		this.item = i;
 	}
 
 	public void attack(Character c) {
-		
-		
+
 	}
-	//returns the item that enemy has dropped, my guess anyways
-	public Item getItem(){
-		EnemyGenerator eg = new EnemyGenerator();
-							
-		return item;
+
+	// returns the item that enemy has dropped, my guess anyways
+	public Item getItem() {
+		EnemyGenerator EG = new EnemyGenerator();
+		Enemy e = EG.generateEnemy(1);
+		if (e.getHp() < 0) {
+			return item;
+		} else
+			return null;
 	}
 
 }
